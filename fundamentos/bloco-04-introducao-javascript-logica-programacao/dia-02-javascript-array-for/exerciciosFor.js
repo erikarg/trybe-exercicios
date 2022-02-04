253caa77bb142fa2a419d7d532d783e990fffc2f
Érika Rocha Gonçalves <erikatrue@gmail.com>
2022-02-04 17:03:54 -0300

    Adiciona exercicios for/of

---
+++ fundamentos/bloco-04-introducao-javascript-logica-programacao/dia-02-javascript-array-for/exerciciosFor.js
@@ -0,0 +1,11 @@
+// Exercicio 1
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+for (let index = 0; index < groceryList.length; index += 1) {
+    console.log(groceryList[index])
+}
+
+// Exercicio 2
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+for (let nomes of names) {
+    console.log(nomes)
+}
