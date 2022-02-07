4dde4bf62e747d2334069986ac7b1227daeed7d7
Érika Rocha Gonçalves <erikatrue@gmail.com>
2022-02-07 20:15:31 -0300

    Adiciona exercicio 2 da Parte 1 do course

--- fundamentos/bloco-04-introducao-javascript-logica-programacao/exerciciosParte1.js
+++ fundamentos/bloco-04-introducao-javascript-logica-programacao/exerciciosParte1.js
@@ -2,8 +2,13 @@ let info = {
     personagem: 'Margarida',
     origem: 'Pato Donald',
     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+    recorrente: 'Sim'
   };
 
 for (let value in info) {
     console.log('Bem-vinda, ' + info.personagem)
+}
+
+for (let object in info) {
+    console.log(object+':', info[object]+';')
 }
