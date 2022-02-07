e095e0f9db491cab6129314fa5ee9ea8ded18085
Érika Rocha Gonçalves <erikatrue@gmail.com>
2022-02-07 18:07:37 -0300

    Adiciona segundo, terceiro e quarto exercicios de fixacao do course

--- fundamentos/bloco-04-introducao-javascript-logica-programacao/dia-04-javascript-objetos-e-funcoes/exerciciosParaFixacao.js
+++ fundamentos/bloco-04-introducao-javascript-logica-programacao/dia-04-javascript-objetos-e-funcoes/exerciciosParaFixacao.js
@@ -5,6 +5,10 @@ let player = {
     medals: { 
         golden: 2, 
         silver: 3},
+    bestInTheWorld: [2006, 2007, 2008, 2009, 2010, 2018],
     };
 
+
 console.log('A jogadora ' + player.name + ' ' + player.lastName + ' tem ' + player.age + ' anos.')
+
+console.log('A jogadora ' + player.name + ' ' + player.lastName + ' foi eleita a melhor do mundo por ' + (player['bestInTheWorld'].length) + ' vezes.');
