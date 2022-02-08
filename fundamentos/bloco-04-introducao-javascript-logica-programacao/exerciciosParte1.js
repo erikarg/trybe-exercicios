345b9b5367ee7934acfaa2ee8cf2f886fa5e336e
Érika Rocha Gonçalves <erikatrue@gmail.com>
2022-02-07 21:09:15 -0300

    Adiciona exercicio 6 da Parte 1 do course

--- fundamentos/bloco-04-introducao-javascript-logica-programacao/exerciciosParte1.js
+++ fundamentos/bloco-04-introducao-javascript-logica-programacao/exerciciosParte1.js
@@ -31,3 +31,20 @@ let info2 = {
 for (let juncao in info, info2) {
     console.log(info[juncao]+ ' e ' + info2[juncao])
 };
+
+let leitor = {
+    nome: 'Julia',
+    sobrenome: 'Pessoa',
+    idade: 21,
+    livrosFavoritos: [
+      {
+        titulo: 'O Pior Dia de Todos',
+        autor: 'Daniela Kopsch',
+        editora: 'Tordesilhas',
+      },
+    ],
+  };
+
+  for (let value in leitor) {
+      console.log('O livro favorito de ' + leitor['nome'] + ' ' + leitor['sobrenome'] + ' se chama ' + "'" + leitor.livrosFavoritos[0].titulo + "'.")
+  }
