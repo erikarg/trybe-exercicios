89162274fb0050c398df02ae97d0ae32b5b68a8b
Érika Rocha Gonçalves <erikatrue@gmail.com>
2022-02-07 21:19:01 -0300

    Adiciona exercicios 7 e 8 da Parte 1 do course

--- fundamentos/bloco-04-introducao-javascript-logica-programacao/exerciciosParte1.js
+++ fundamentos/bloco-04-introducao-javascript-logica-programacao/exerciciosParte1.js
@@ -47,4 +47,16 @@ let leitor = {
 
   for (let value in leitor) {
       console.log('O livro favorito de ' + leitor['nome'] + ' ' + leitor['sobrenome'] + ' se chama ' + "'" + leitor.livrosFavoritos[0].titulo + "'.")
-  }
+  };
+
+leitor.livrosFavoritos.push( {
+    titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+    autor: 'JK Rowling',
+    editora: 'Rocco',
+  });
+
+console.log(leitor);
+
+for (let livros in leitor) {
+    console.log('Julia tem ' + leitor.livrosFavoritos.length+ ' livros favoritos.')
+};
