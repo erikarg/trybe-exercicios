99ca938d73729545c5bc2409b3d955f67086c622
Érika Rocha Gonçalves <erikatrue@gmail.com>
2022-02-08 13:24:42 -0300

    Adiciona exercicio 4 da parte 2 do course

--- fundamentos/bloco-04-introducao-javascript-logica-programacao/exerciciosParte2.js
+++ fundamentos/bloco-04-introducao-javascript-logica-programacao/exerciciosParte2.js
@@ -37,3 +37,14 @@ function verificaMenorIndice (numeros) {
 }
 console.log(verificaMenorIndice(array2))
 
+let arrayDeNomes = ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'];
+function verificaMaisCaracteres (palavras) {
+    let maiorPalavra = palavras[0];
+    for (let index in palavras) {
+        if (maiorPalavra.length < palavras[index].length) {
+            maiorPalavra = palavras[index];
+    }
+}
+return maiorPalavra;
+}
+console.log(verificaMaisCaracteres(['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana']));
