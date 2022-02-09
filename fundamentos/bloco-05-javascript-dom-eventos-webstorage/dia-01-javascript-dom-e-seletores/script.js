7a5a99b527f9685a4ee5c181e6ff496c4b0c478c
Érika Rocha Gonçalves <erikatrue@gmail.com>
2022-02-09 20:01:05 -0300

    Adiciona exercicios iniciais

---
+++ fundamentos/bloco-05-javascript-dom-eventos-webstorage/dia-01-javascript-dom-e-seletores/script.js
@@ -0,0 +1,14 @@
+let div1 = document.getElementById('emergency-tasks .div'); 
+div1.style.backgroundColor = '#41197f';
+
+let id1 = document.getElementById('header-container');
+id1.style.backgroundColor = '#2fc18c';
+
+let class1 = document.getElementsByClassName('emergency-tasks');
+class1.style.backgroundColor = '#ffbec7';
+
+let id2 = document.getElementById('footer-container');
+id2.style.backgroundColor = '#023031';
+
+
+document.getElementById('header-container').style.backgroundColor = "#2fc18c";
