09831c11a1d8601847a9132549d962531dfc0b6c
Érika Rocha Gonçalves <erikatrue@gmail.com>
2022-02-10 08:56:56 -0300

    Adiciona exercicios de fixacao de DOM

--- fundamentos/bloco-05-javascript-dom-eventos-webstorage/dia-01-javascript-dom-e-seletores/script.js
+++ fundamentos/bloco-05-javascript-dom-eventos-webstorage/dia-01-javascript-dom-e-seletores/script.js
@@ -1,6 +1,3 @@
-let div1 = document.getElementById('emergency-tasks .div'); 
-div1.style.backgroundColor = '#41197f';
-
 let id1 = document.getElementById('header-container');
 id1.style.backgroundColor = '#2fc18c';
 
@@ -10,5 +7,8 @@ class1.style.backgroundColor = '#ffbec7';
 let id2 = document.getElementById('footer-container');
 id2.style.backgroundColor = '#023031';
 
+let class2 = document.getElementsByClassName('standard-screenshot');
 
 document.getElementById('header-container').style.backgroundColor = "#2fc18c";
+
+let class3 = document.getElementsByClassName('emergency-tasks'); 
