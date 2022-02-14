80dcd04f88d1d84d302594b368748135171ccdf0
Érika Rocha Gonçalves <erikatrue@gmail.com>
2022-02-14 19:26:34 -0300

    Adiciona exercicio de hoje

--- fundamentos/bloco-05-javascript-dom-eventos-webstorage/dia-04-javascript-web-storage/script.js
+++ fundamentos/bloco-05-javascript-dom-eventos-webstorage/dia-04-javascript-web-storage/script.js
@@ -1,5 +1,32 @@
-function alteraFundo () {
-    let cor = document.querySelector('#bgcolor')
-    document.getElementById('meuFundo').style.backgroundColor = cor;
-    
-}
+let colors = ['green', 'violet', 'red', 'yellow', 'orange', 'pink', 'brown','blue', 'white', 'black']
+
+let button1 = document.getElementById('botao');
+
+button1.addEventListener('click', function(){
+    var randomColor = colors [Math.floor(Math.random()*colors.length)]
+    let back = document.getElementById('bg');
+    back.style.background = randomColor;
+})
+
+let colors2 = ['white', 'red', 'grey', 'yellow', 'black', 'brown', 'green']
+
+let button2 = document.getElementById('botao2'); 
+
+button2.addEventListener('click', function() {
+    var aleatoria = colors2 [Math.floor(Math.random()*colors2.length)]
+    let text1 = document.getElementsByClassName('text0');
+    for (i = 0; i < text1.length; i++) {
+        text1.item(i).style.color = aleatoria;
+    }
+})
+
+// Ajustar abaixo, ainda não funciona
+let estilosDaFonte = ['Impact, Charcoal, sans-serif', 'Lucida Sans, Lucida Grande, Verdana']
+
+let button3 = document.getElementById('botao3');
+
+button3.addEventListener('click', function() {
+    var aleatoria2 = estilosDaFonte [Math.floor(Math.random()*estilosDaFonte.length)]
+    let text5 = document.getElementsByClassName('text0');
+        text5.style.fontFamily = aleatoria2;
+    })
