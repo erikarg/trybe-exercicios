f565f2619005faadc54e4de6857e7b2b56ed03c6
Érika Rocha Gonçalves <erikatrue@gmail.com>
2022-02-18 15:48:18 -0300

    Adiciona alterações

--- fundamentos/bloco-05-javascript-dom-eventos-webstorage/dia-04-javascript-web-storage/script.js
+++ fundamentos/bloco-05-javascript-dom-eventos-webstorage/dia-04-javascript-web-storage/script.js
@@ -1,17 +1,13 @@
 let colors = ['green', 'violet', 'red', 'yellow', 'orange', 'pink', 'brown','blue', 'white', 'black']
-
 let button1 = document.getElementById('botao');
-
 button1.addEventListener('click', function(){
     var randomColor = colors [Math.floor(Math.random()*colors.length)]
     let back = document.getElementById('bg');
-    back.style.background = randomColor;
+    back.style.background = randomColor;    
 })
 
 let colors2 = ['white', 'red', 'grey', 'yellow', 'black', 'brown', 'green']
-
 let button2 = document.getElementById('botao2'); 
-
 button2.addEventListener('click', function() {
     var aleatoria = colors2 [Math.floor(Math.random()*colors2.length)]
     let text1 = document.getElementsByClassName('text0');
@@ -20,13 +16,16 @@ button2.addEventListener('click', function() {
     }
 })
 
-// Ajustar abaixo, ainda não funciona
-let estilosDaFonte = ['Impact, Charcoal, sans-serif', 'Lucida Sans, Lucida Grande, Verdana']
-
-let button3 = document.getElementById('botao3');
-
-button3.addEventListener('click', function() {
-    var aleatoria2 = estilosDaFonte [Math.floor(Math.random()*estilosDaFonte.length)]
-    let text5 = document.getElementsByClassName('text0');
-        text5.style.fontFamily = aleatoria2;
-    })
+let tamanhoDaLetra1 = document.querySelector('input[name="tamanhoDaLetra"]')
+tamanhoDaLetra1.addEventListener('change', function(){
+    let text1 = document.getElementById('textinho');
+    let text2 = document.getElementById('textinho2');
+    let text3 = document.getElementById('textinho3');
+    let text4 = document.getElementById('textinho4');
+    let text5 = document.getElementById('textinho5');
+    text1.style.fontSize = `${tamanhoDaLetra1.value}px`
+    text2.style.fontSize = `${tamanhoDaLetra1.value}px`
+    text3.style.fontSize = `${tamanhoDaLetra1.value}px`
+    text4.style.fontSize = `${tamanhoDaLetra1.value}px`
+    text5.style.fontSize = `${tamanhoDaLetra1.value}px`
+});
