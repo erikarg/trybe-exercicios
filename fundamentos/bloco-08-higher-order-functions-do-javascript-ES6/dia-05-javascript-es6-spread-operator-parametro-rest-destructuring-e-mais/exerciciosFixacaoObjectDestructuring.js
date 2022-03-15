c0332186bf2829a0dd5fa81edeb74cef0a934ba7
Érika Rocha Gonçalves <erikatrue@gmail.com>
2022-03-15 14:51:09 -0300

    Adiciona mais exercicios de fixacao do Object Destructuring

--- fundamentos/bloco-08-higher-order-functions-do-javascript-ES6/dia-05-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exerciciosFixacaoObjectDestructuring.js
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-ES6/dia-05-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exerciciosFixacaoObjectDestructuring.js
@@ -13,3 +13,22 @@ console.log(weekdays); // ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday
 
 const weekdays2 = [...workDays, 'Feriado Aleatório', ...weekend];
 console.log(weekdays2);
+
+const user = {
+  name: 'Maria',
+  age: 21,
+  nationality: 'Brazilian',
+};
+
+const jobInfos = {
+  profession: 'Software engineer',
+  squad: 'Rocket Landing Logic',
+  squadInitials: 'RLL',
+};
+
+const terceiroObjeto = {
+  ...user,
+  ...jobInfos,
+};
+
+console.log(`Hello, I'm ${user.name}, I'm ${user.age} years old and I'm ${user.nationality}. I work as a ${jobInfos.profession} and my squad is ${jobInfos.squadInitials} - ${jobInfos.squad}.`);
