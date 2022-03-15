15989364b8b3678386c42b0f57388e101d93bf77
Érika Rocha Gonçalves <erikatrue@gmail.com>
2022-03-15 17:19:55 -0300

    Adiciona exercicio 3 de fixacao do array destructuring

--- fundamentos/bloco-08-higher-order-functions-do-javascript-ES6/dia-05-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exerciciosFixacaoArrayDestructuring.js
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-ES6/dia-05-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exerciciosFixacaoArrayDestructuring.js
@@ -18,3 +18,10 @@ let bebida = 'arroz';
 // Utilizando array destructuring, faça com que os valores apareçam nas variáveis correspondentes ao seu verdadeiro tipo
 [comida, animal, bebida] = [bebida, comida, animal];
 console.log(comida, animal, bebida);
+
+// Exercicio 3
+let numerosPares = [1, 3, 5, 6, 8, 10, 12];
+ // [6, 8, 10, 12];
+// Utilize array destructuring para produzir o resultado esperado pelo console.log abaixo
+[,,, ...numerosPares] = numerosPares;
+console.log(numerosPares);
