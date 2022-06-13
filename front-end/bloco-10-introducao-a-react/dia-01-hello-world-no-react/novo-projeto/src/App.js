d4bbf912c9775edebd306d7c2c8ad846df68e521
Érika Rocha Gonçalves <erikatrue@gmail.com>
2022-06-13 17:00:48 -0300

    Adicionando exercicio 3

---
+++ front-end/bloco-10-introducao-a-react/dia-01-hello-world-no-react/novo-projeto/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Header from './Header';
+import React from 'react';
+import Content from './Content';
+import Footer from './Footer';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Content />
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ front-end/bloco-10-introducao-a-react/dia-01-hello-world-no-react/novo-projeto/src/Content.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+const conteudos = [
+  {
+    conteudo: 'High Order Functions',
+    bloco: 8,
+    status: 'Aprendido'
+  },
+  {
+    conteudo: 'Composicao de Componentes',
+    bloco: 11,
+    status: 'Aprendendo',
+  },
+  {
+    conteudo: 'Composicao de Estados',
+    bloco: 12,
+    status: 'Aprenderei'
+  },
+  {
+    conteudo: 'Redux',
+    bloco: 16,
+    status: 'Aprenderei'
+  },
+];
+
+class Content extends React.Component {
+  render() {
+    return (
+      <div className='content'>
+        {conteudos.map((items) => (
+          <div key={items.conteudo} className='father'>
+            <h3>{`O conteúdo é: ${items.conteudo}`}</h3>
+            <p>{`Status: ${items.status}`}</p>
+            <p>{`Bloco: ${items.bloco}`}</p>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Content
