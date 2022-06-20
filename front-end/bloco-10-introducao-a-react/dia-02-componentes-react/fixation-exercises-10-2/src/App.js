184931cfb35416c36f9798df62ad2da13a098e95
Érika Rocha Gonçalves <erikatrue@gmail.com>
2022-06-20 16:18:29 -0300

    Adicionando exercicios de fixacao parte 3

--- front-end/bloco-10-introducao-a-react/dia-02-componentes-react/fixation-exercises-10-2/src/App.js
+++ front-end/bloco-10-introducao-a-react/dia-02-componentes-react/fixation-exercises-10-2/src/App.js
@@ -15,38 +15,71 @@
 
 // Parte 2 - Exercicios de Fixacao
 
-// arquivo App.js, criado pelo create-react-app, modificado
+// import React from 'react';
+// import './App.css';
+// import Order from './Order';
+
+// class App extends React.Component {
+//   render() {
+//     const headphone = {
+//       id: 102,
+//       user: "cena@gmail.com",
+//       product: "Razer Headphone",
+//       price: {
+//         value: 99.99,
+//         currency: "dollars"
+//       }
+//     };
+
+//     const energyDrink = {
+//       id: 77,
+//       user: "cena@gmail.com",
+//       product: "Monster 500mL",
+//       price: {
+//         value: 9.99,
+//         currency: "dollars"
+//       }
+//     };
+
+//     return (
+//       <div className="App">
+//         <h1> Orders recently created </h1>
+//       <Order order={headphone} />
+//       <Order order={energyDrink} />
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
+
+// Parte 3 - Exercicios de Fixacao
+
 import React from 'react';
 import './App.css';
-import Order from './Order';
+import UserProfile from './UserProfile';
 
 class App extends React.Component {
   render() {
-    const headphone = {
-      id: 102,
-      user: "cena@gmail.com",
-      product: "Razer Headphone",
-      price: {
-        value: 99.99,
-        currency: "dollars"
+    const users = [
+      {
+        id: 102,
+        name: "João",
+        email: "joao@gmail.com",
+        avatar: "https://cdn.pixabay.com/photo/2016/08/20/05/38/avatar-1606916_640.png"
+      },
+      {
+        id: 77,
+        name: "Amélia",
+        email: "amelia@gmail.com",
+        avatar: "https://cdn.pixabay.com/photo/2017/01/31/13/05/cameo-2023867_640.png"
       }
-    };
-
-    const energyDrink = {
-      id: 77,
-      user: "cena@gmail.com",
-      product: "Monster 500mL",
-      price: {
-        value: 9.99,
-        currency: "dollars"
-      }
-    };
+    ];
 
     return (
       <div className="App">
-        <h1> Orders recently created </h1>
-      <Order order={headphone} />
-      <Order order={energyDrink} />
+        {users.map(user => 
+        <UserProfile user={user} key={user.id} />)}
       </div>
     );
   }
