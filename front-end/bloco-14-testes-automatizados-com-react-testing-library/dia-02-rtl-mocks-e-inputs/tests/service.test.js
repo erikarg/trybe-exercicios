d91f4364139edf92d93e8e5bbb4d36e464da754d
Érika Rocha Gonçalves <erikatrue@gmail.com>
2022-07-14 18:02:03 -0300

    Adicionando exercício 3

--- front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-02-rtl-mocks-e-inputs/tests/service.test.js
+++ front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-02-rtl-mocks-e-inputs/tests/service.test.js
@@ -30,3 +30,16 @@ test('Testa se a função foi chamada, se a sua nova implementação foi aplicad
   expect(randomNumber(15,3)).toBe(undefined);
   expect(randomNumber).toHaveBeenCalledTimes(1);
 });
+
+// Exercício 3
+
+test('Testa se a nova implementação recebe três parâmetros, retorna a multiplicação deles, reseta a implementação e crie uma nova que recebe um parâmetro e retorna o dobro', () => {
+  randomNumber = jest.fn().mockImplementation((a, b, c) => a * b * c);
+  randomNumber(2, 5, 9);
+  expect(randomNumber(2, 5, 9)).toBe(90);
+
+  randomNumber.mockReset();
+  randomNumber = jest.fn().mockImplementation((a) => a * a);
+  expect(randomNumber(235)).toBe(55225);
+  expect(randomNumber).toHaveBeenCalledTimes(1);
+});
